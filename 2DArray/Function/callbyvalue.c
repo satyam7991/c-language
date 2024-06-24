532651d3c3cfdf9b2fc28bcc5dc807174c211b5f
Satyam Singh <147079047+satyam7991@users.noreply.github.com>
2024-06-24 23:01:07 +0530

    a

--- 2DArray/Function/callbyvalue.c
+++ 2DArray/Function/callbyvalue.c
@@ -3,9 +3,9 @@ void swap(int,int);
 int main ()
 {
     int n1=20, n2=50;
-    printf("N1=%d and N2=%d\n",n1,n2);
+    printf("n1=%d and n2=%d\n",n1,n2);
     swap(n1,n2);//call by value
-    printf("N1=%d and N2=%d\n",n1,n2);
+    printf("n1=%d and n2=%d\n",n1,n2);
     return 0;
 }
 void swap(int n1, int n2)
@@ -13,5 +13,5 @@ void swap(int n1, int n2)
     int temp=n1;
     n1=n2;
     n2=temp;
-    printf("N1=%d and N2=%d\n",n1,n2);
+    printf("n1=%d and n2=%d\n",n1,n2);
 }
