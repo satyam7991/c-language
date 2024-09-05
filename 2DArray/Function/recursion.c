7a69cce767c8618ba898d1ecf4e6c69d180f94b4
Satyam Singh <147079047+satyam7991@users.noreply.github.com>
2024-09-05 21:45:26 +0530

    recursion

---
+++ 2DArray/Function/recursion.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int square(int n);
+int main(){
+    int n=5;
+    printf("%d",square(n));
+    return 0;
+}
+int square(int n){
+    if(n==0||n==1){
+        return 1;
+    }
+    else{
+        return n*n;
+    }
+}
